14fd5ad4e4d858ab137d48323f9973b6411028ec
malexit240 <43965923+malexit240@users.noreply.github.com>
2018-10-25 19:40:45 +0300

    Initial commit

---
+++ DBProcessing.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Data.SqlTypes;
+using System.Data.Sql;
+
+namespace TaskPlanner
+{
+    class DBProcessing
+    {
+        TreeNode root;
+        public DBProcessing()
+        {
+            //createDB();
+            
+
+        }
+
+        public void connectTree(TreeNode root)
+        {
+            this.root = root;
+        }
+
+        private void createDB()
+        {
+            SQLiteConnection conn = new SQLiteConnection("Data Source=Database.db; Version=3;");
+            conn.Open();
+
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                SQLiteCommand cmd = conn.CreateCommand();
+                string sql_command =
+                    "CREATE TABLE TASKS(" +
+                    "id integer primary key autoincrement," +
+                    "task varchar(50),parent_id integer," +
+                    "foreign key(parent_id) references TASKS(id));" +
+                    "CREATE TABLE TASKSINFO(" +
+                    "id integer," +
+                    "notation varchar(500)," +
+                    "complete integer," +
+                    "foreign key(id) references TASKS(id)" +
+                    ");" +
+                    "INSERT INTO TASKS(task,parent_id) VALUES ('root',1)";
+                cmd.CommandText = sql_command;
+                cmd.ExecuteNonQuery();
+                conn.Dispose();
+            }
+
+        }
+
+        public void addTask(string taskname, int parent_id)
+        {
+            SQLiteConnection conn = new SQLiteConnection("Data Source=Database.db; Version=3;");
+            conn.Open();
+
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                SQLiteCommand cmd = conn.CreateCommand();
+                string sql_command = $"INSERT INTO TASKS(task,parent_id) VALUES('{taskname}','{parent_id}')";
+                cmd.CommandText = sql_command;
+                cmd.ExecuteNonQuery();
+                conn.Dispose();
+            }
+        }
+
+        private void getTasksFromParent(int parent_id)
+        {
+
+            SQLiteConnection conn = new SQLiteConnection("Data Source=Database.db; Version=3;");
+            conn.Open();
+
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                SQLiteCommand cmd = conn.CreateCommand();
+                string sql_command = $"SELECT id, task, parent_id FROM TASKS WHERE parent_id = {parent_id};";
+                cmd.CommandText = sql_command;
+
+                SQLiteDataReader r = cmd.ExecuteReader();
+
+                while (r.Read())
+                {
+                    // result.Add(r["id"] + " " + r["task"] + " " + r["parent_id"]);
+                }
+                r.Close();
+                conn.Dispose();
+            }
+
+        }
+
+        public void fillTree(TreeNode root)
+        {
+            SQLiteConnection conn = new SQLiteConnection("Data Source=Database.db; Version=3;");
+            conn.Open();
+
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                SQLiteCommand cmd = conn.CreateCommand();
+                string sql_command = "SELECT id, task, parent_id FROM TASKS order by parent_id;";
+                cmd.CommandText = sql_command;
+
+                SQLiteDataReader r = cmd.ExecuteReader();
+
+                TreeNode local = null;
+                int id,parent_id;
+                string task;
+                while (r.Read())
+                {
+                    id = Convert.ToInt32(r["id"]);
+                    parent_id = Convert.ToInt32(r["parent_id"]);
+                    task = r["task"].ToString();
+                    if (id == 1) continue;
+                    local = root.getNodeById(parent_id);
+                    local.addChild(new TreeNode(local, id,task));
+
+                }
+                r.Close();
+                conn.Dispose();
+            }
+        }
+
+        public int getIdByNameAndParent(string taskname,int parent_id)
+        {
+            
+
+            SQLiteConnection conn = new SQLiteConnection("Data Source=Database.db; Version=3;");
+            conn.Open();
+
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                SQLiteCommand cmd = conn.CreateCommand();
+                string sql_command = $"SELECT id FROM TASKS WHERE task = '{taskname}' AND parent_id = {parent_id};";
+                cmd.CommandText = sql_command;
+
+                SQLiteDataReader r = cmd.ExecuteReader();
+
+                while (r.Read())
+                {
+                    return Convert.ToInt32(r["id"]);
+                }
+                r.Close();
+                conn.Dispose();
+            }
+
+            return 0;
+        }
+    }
+
+    public class TreeNode
+    {
+        public TreeNode parent;
+        public List<TreeNode> child;
+        public int Id { get; }
+        public string Task { get; }
+
+        public TreeNode(TreeNode parent, int id, string task)
+        {
+            this.parent = parent;
+            this.Id = id;
+            this.Task = task;
+            this.child = new List<TreeNode>();
+        }
+
+
+
+        public void addChild(TreeNode child)
+        {
+            this.child.Add(child);
+        }
+
+        public TreeNode getNodeById(int id, bool isLocal = false)
+        {
+            if (this.Id == id) return this;
+
+            TreeNode result;
+            for (int i = 0; i < child.Count; i++)
+            {
+                if (child[i].Id == id) return child[i];
+                if (!isLocal)
+                {
+                    result = child[i].getNodeById(id);
+                    if (result != null) return result;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskPlanner
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            
+            InitializeComponent();
+        }
+
+        private void addTree_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.addNode(this.taskname.Text, Convert.ToInt32(this.parent_id.Text));
+            UpdateTree();
+        }
+
+        private void UpdateTree()
+        {
+            TreeNode root = ViewModel.root;
+            string tree = addText(root, 0);
+
+            Tree.Text = tree;
+        }
+
+        private string addText(TreeNode root,int deep)
+        {
+            string result=""+'\n';
+            for (int i = 0; i < deep; i++)
+            {
+                result += '\t';
+            }
+            result += root.Task+" "+root.Id.ToString();
+            for (int i = 0; i < root.child.Count; i++)
+            {
+                result += addText(root.child[i], deep + 1);
+            }
+            result += '\n';
+            return result;
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            TreeNode root = ViewModel.root;
+            string tree = addText(root, 0);
+
+            Tree.Text = tree;
+        }
+    }
+
+    public static class ViewModel
+    {
+        public static TreeNode root;
+        private static DBProcessing db;
+        static ViewModel()
+        {
+            root = new TreeNode(null, 1, "root");
+            db = new DBProcessing();
+            db.connectTree(root);
+            db.fillTree(root);
+
+        }
+
+        public static void addNode(string taskname,int parent_id)
+        {
+            db.addTask(taskname, parent_id);
+            int id = db.getIdByNameAndParent(taskname, parent_id);
+            TreeNode parent = root.getNodeById(parent_id);
+            parent.addChild(new TreeNode(parent, id, taskname));
+        }
+
+    }
+
+   
+
+    
+
+}
